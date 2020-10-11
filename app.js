c31ccbe0892797cbabe2e9aab2474d03b3eb947a
luke-h1 <luke.howsam@yahoo.com>
2020-10-11 20:24:15 +0100

    make some improvements

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 const form = document.getElementById('form-control');
 const outputDiv = document.getElementById('card');
+const loaderImg = document.getElementById('loader');
+
+function showLoader() {
+  loaderImg.style.display = 'block';
+}
+
+function hideLoader() {
+  loaderImg.style.display = 'none';
+}
+
+function clearFields() {
+  const postcodeValue = document.getElementById('input');
+  postcodeValue.value = '';
+}
 
 async function getData(e) {
+  showLoader();
   e.preventDefault();
   const postcodeValue = document.getElementById('input');
   const postcode = postcodeValue.value;
   const BASE_URL = `https://cors-anywhere.herokuapp.com/https://www.lockdownapi.com/${postcode}`;
   const res = await fetch(`${BASE_URL}`);
   const json = await res.json();
+  clearFields();
   showData(json);
 }
 
 function showData(json) {
+  hideLoader();
   let output = '';
   output += `
   <h1 class="card-title">${json.admindistrictname}</h1> 
-  <h1 class="card-title-secondary">${json.councilname}</h1>
-
   <p class="item"> LockDown Status: 
   ${
     json.lockdowstatus === 'areaofconcern' ||
     'areaofintervention' ||
     'nationlockdown'
-      ? `<span class="bad">${json.lockdownstatus}</span>`
-      : `<span class="good">${json.lockdownstatus}</span>`
+      ? `<span class="bad"><br>${json.lockdownstatus} ❌</span>`
+      : `<span class="good">${json.lockdownstatus} ✅</span>`
   }   
 </p> 
 
@@ -31,8 +46,8 @@ function showData(json) {
 Non Essential Shops: 
 ${
   json.nonessentialshops === 'false'
-    ? `<span class="bad">${json.nonessentialshops}</span>`
-    : `<span class="good">${json.nonessentialshops}</span>`
+    ? `<span class="bad">${json.nonessentialshops}❌</span>`
+    : `<span class="good">${json.nonessentialshops} ✅</span>`
 }  
 </p>
 
@@ -40,8 +55,8 @@ ${
 Two HouseHolds: 
 ${
   json.twohouseholds === 'false'
-    ? `<span class="bad">${json.twohouseholds}</span>`
-    : `<span class="good">${json.twohouseholds}</span>`
+    ? `<span class="bad">${json.twohouseholds} ❌</span>`
+    : `<span class="good">${json.twohouseholds} ✅</span>`
 }
 </p> 
 
@@ -49,8 +64,8 @@ ${
 Gyms & Fitness : 
 ${
   json.gymsandfitness === 'false'
-    ? `<span class="bad">${json.gymsandfitness}</span>`
-    : `<span class="good">${json.gymsandfitness}</span>`
+    ? `<span class="bad">${json.gymsandfitness} ❌</span>`
+    : `<span class="good">${json.gymsandfitness} ✅</span>`
 }
 </p> 
 
@@ -58,8 +73,8 @@ ${
 Meet Outdoors : 
 ${
   json.meetoutdoors === 'false'
-    ? `<span class="bad">${json.meetoutdoors}</span>`
-    : `<span class="good">${json.meetoutdoors}</span> `
+    ? `<span class="bad">${json.meetoutdoors} ❌</span>`
+    : `<span class="good">${json.meetoutdoors} ✅</span> `
 }
 </p>
 
@@ -67,8 +82,8 @@ ${
 Pubs & Restaurants : 
 ${
   json.pubsandrestuarants === 'false'
-    ? `<span class="bad">${json.pubsandrestuarants}</span>`
-    : `<span class="good">${json.pubsandrestuarants}</span>`
+    ? `<span class="bad">${json.pubsandrestuarants} ❌</span>`
+    : `<span class="good">${json.pubsandrestuarants} ✅</span>`
 }
 </p>
 
